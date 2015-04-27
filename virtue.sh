5f3490421febe1889025655e5ac2ce3688759392
Jason Agnew <jason@bigbitecreative.com>
2015-04-27 21:06:32 +0100

    Cron fix

--- virtue.sh
+++ virtue.sh
@@ -476,7 +476,7 @@ EOF
 
     if [ $TYPE = "delete" ] || [ $TYPE = "update" ]; then
       #Remove
-      crontab -l 2>/dev/null | sed -e "\?^$JOB_NAME\$?,/^\$/ d" | crontab -
+      crontab -l 2>/dev/null | sed -e "/#$JOB_NAME$/d" | crontab -
     fi
 
     if [ $TYPE = "add" ] || [ $TYPE = "update" ]; then
