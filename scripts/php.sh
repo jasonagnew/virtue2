55f69f5ef6f419b477c5b712444ef2637e7e5353
Jason Agnew <jason@bigbitecreative.com>
2016-06-06 23:01:17 +0100

    Change PHP apt repo

--- scripts/php.sh
+++ scripts/php.sh
@@ -4,25 +4,25 @@
 source ~/.virtue_config
 
   echo ">>> Installing PHP"
- 
-    if [ $PHP_VERISON == "latest" ]; then
-        sudo add-apt-repository -y ppa:ondrej/php5-5.6 > /dev/null 2>&1
-    fi
-    
-    if [ $PHP_VERISON == "distributed" ]; then
-        sudo add-apt-repository -y ppa:ondrej/php5 > /dev/null 2>&1
-    fi
 
-    if [ $PHP_VERISON == "previous" ]; then
-        sudo add-apt-repository -y ppa:ondrej/php5-oldstable > /dev/null 2>&1
-    fi
+    sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php > /dev/null 2>&1
 
     sudo apt-key update > /dev/null 2>&1
     sudo apt-get update > /dev/null 2>&1
-
+    
     # Install PHP
     # -qq implies -y --force-yes
-    sudo apt-get install -qq --force-yes php5-cli php5-fpm php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached php5-imagick php5-intl libssh2-1-dev libssh2-php > /dev/null 2>&1
+    if [ $PHP_VERISON == "latest" ]; then
+        sudo apt-get install -qq --force-yes php7.0-cli php7.0-fpm php7.0-mysql php7.0-pgsql php7.0-sqlite php7.0-curl php7.0-gd php7.0-gmp php7.0-mcrypt php7.0-xdebug php7.0-memcached php7.0-imagick php7.0-intl libssh2-1-dev libssh2-php > /dev/null 2>&1
+    fi
+    
+    if [ $PHP_VERISON == "distributed" ]; then
+        sudo apt-get install -qq --force-yes php5.6-cli php5.6-fpm php5.6-mysql php5.6-pgsql php5.6-sqlite php5.6-curl php5.6-gd php5.6-gmp php5.6-mcrypt php5.6-xdebug php5.6-memcached php5.6-imagick php5.6-intl libssh2-1-dev libssh2-php > /dev/null 2>&1
+    fi
+
+    if [ $PHP_VERISON == "previous" ]; then
+        sudo apt-get install -qq --force-yes php5.5-cli php5.5-fpm php5.5-mysql php5.5-pgsql php5.5-sqlite php5.5-curl php5.5-gd php5.5-gmp php5.5-mcrypt php5.5-xdebug php5.5-memcached php5.5-imagick php5.5-intl libssh2-1-dev libssh2-php > /dev/null 2>&1
+    fi
 
     # Set PHP FPM to listen on TCP instead of Socket
     sudo sed -i "s/listen =.*/listen = 127.0.0.1:9000/" /etc/php5/fpm/pool.d/www.conf
